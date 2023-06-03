aa8261aca766154404c05342ced1b16800a7834b
yuval2017 <hitter@post.bgu.ac.il>
2023-06-03 20:23:19 +0300

    add waze link

--- ClientUI/src/components/Links.js
+++ ClientUI/src/components/Links.js
@@ -1,6 +1,13 @@
 import React from "react";
 export default function Links(){
   return(
-    <a href="https://api.whatsapp.com/send?phone=+972524436774&text=New Order" target="_blank" rel="noreferrer">Open WhatsApp</a>
+    <div>
+      <a href="https://api.whatsapp.com/send?phone=+972524436774&text=New Order" target="_blank" rel="noreferrer">Open WhatsApp</a>
+      <a href="https://www.waze.com/ul?q=פרחי אס&navigate=yes" target="_blank" rel="noreferrer">Open Waze</a>
+
+
+
+    </div>
+    
   );
 }
