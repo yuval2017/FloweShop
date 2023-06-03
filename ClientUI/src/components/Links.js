5944214819f47779e7a1132ebcf542fb4f66ad6c
yuval2017 <hitter@post.bgu.ac.il>
2023-06-03 20:28:28 +0300

    fix waze link

--- ClientUI/src/components/Links.js
+++ ClientUI/src/components/Links.js
@@ -3,7 +3,8 @@ export default function Links(){
   return(
     <div>
       <a href="https://api.whatsapp.com/send?phone=+972524436774&text=New Order" target="_blank" rel="noreferrer">Open WhatsApp</a>
-      <a href="https://www.waze.com/ul?q=פרחי אס&navigate=yes" target="_blank" rel="noreferrer">Open Waze</a>
+      <a href="https://www.waze.com/ul?ll=0,0&q=פרחי אס" target="_blank" rel="noreferrer">Open Waze</a>
+
 
 
 
